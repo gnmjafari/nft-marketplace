87f50129b2493681dfdbffe8b6160adcccf52b9d
gnmjafari <gnm.jafaria@gmail.com>
2024-09-11 17:49:29 +0330

    create navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 overflow-hidden 
+        min-h-screen`}
       >
         {children}
       </body>

df874a3d8587d9fd1e323ae77984495988390c19
gnmjafari <gnm.jafaria@gmail.com>
2024-09-21 19:12:26 +0330

    Mint token function and test js

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -6,5 +6,20 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NftMarket is ERC721URIStorage {
+    uint256 private _listedItems;
+    uint256 private _tokenIds;
+
     constructor() ERC721("CreaturesNFT", "CNFT") {}
+
+    function mintToken(string memory tokenURI) public payable returns (uint) {
+        _tokenIds++;
+        _listedItems++;
+
+        uint newTokenId = _tokenIds;
+
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        return newTokenId;
+    }
 }
---
+++ test/nftMarket.test.js
@@ -0,0 +1,28 @@
+const NftMarket = artifacts.require("NftMarket");
+
+contract("NftMarket", (accounts) => {
+  let _contract = null;
+
+  before(async () => {
+    _contract = await NftMarket.deployed();
+    console.log("accounts", accounts);
+  });
+
+  describe("Mint token", () => {
+    const tokenURI = "https://test.com";
+    before(async () => {
+      await _contract.mintToken(tokenURI, {
+        from: accounts[0],
+      });
+    });
+    it("owner of first token should be address[0]", async () => {
+      const owner = await _contract.ownerOf(1);
+
+      assert.equal(
+        owner,
+        accounts[0],
+        "owner of token is not matching address[0]"
+      );
+    });
+  });
+});
